7b0e3eb96b1b3c64db945dc6bc2731acdde65c27
xavier77dev <xavier.basurto.77@gmail.com>
2025-03-07 22:39:32 -0500

    modify: component ModalBase in shared

--- src/modules/dashboard/products/pages/ProductsPage.vue
+++ src/modules/dashboard/products/pages/ProductsPage.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-  import ModalCreateProduct from "../components/ModalCreateProduct.vue";
-  import ProductsList from "../components/ProductList.vue";
+import BaseModal from "@/shared/components/ui/BaseModal.vue";
+import ProductsList from "../components/ProductList.vue";
+import FormCreateProduct from "../components/FormCreateProduct.vue";
+import { ref } from "vue";
+
+const showDialog = ref(false);
+
 </script>
 
 <template>
   <div class="flex justify-end mr-4">
-    <ModalCreateProduct />
+
+    <BaseModal title="Nuevo Producto" v-model="showDialog" @open-dialog="showDialog = true">
+      <FormCreateProduct @closed-dialog="showDialog = false" />
+    </BaseModal>
+
   </div>
 
   <ProductsList />
---
+++ src/shared/components/BaseModal.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+const showDialog = defineModel<boolean>()
+
+interface Props {
+    title: string
+}
+
+const props = defineProps<Props>();
+
+type Emits = (e: "open-dialog") => void;
+
+const emit = defineEmits<Emits>();
+
+</script>
+
+<template>
+    <div class="q-pa-md q-gutter-sm">
+        <q-btn no-caps color="primary" :label="props.title" @click="emit('open-dialog')" />
+
+        <q-dialog v-model="showDialog" showkdrop-filter="blur(4px) saturate(150%)">
+            <q-card>
+
+                <slot />
+
+            </q-card>
+        </q-dialog>
+    </div>
+</template>
